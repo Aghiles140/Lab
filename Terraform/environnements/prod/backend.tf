257264cda246035863b1d0e8e755ad813eca0c79
Aghiles HAMITI <ghilashamiti@gmail.com>
2025-07-03 12:38:38 +0200

    Update backend.tf prod

--- Terraform/environnements/prod/backend.tf
+++ Terraform/environnements/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "azurerm" {
-      resource_group_name = "rg-dev-infra" #RG qui contient le str account
+      resource_group_name = "rg-prod-infra" #RG qui contient le str account
       storage_account_name = "stracctfstate213"
       container_name = "tfstate" # container pour stocker les state files
-      key = "dev.tfstate" #Blob qui sera crée dans le container
+      key = "prod.tfstate" #Blob qui sera crée dans le container
     }
     
 }

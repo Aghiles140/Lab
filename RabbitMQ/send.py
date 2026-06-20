d0f794420a793fdb54a2ff72fe6ab90847852732
Aghiles <aghiles@Mac.lan>
2026-06-20 11:49:56 +0200

    Corrected input

--- RabbitMQ/send.py
+++ RabbitMQ/send.py
@@ -5,10 +5,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-def callback(ch, method, properties, body):
-	print(f" [x] Received {body}")
-
-channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-
-print(' [*] Waiting for messages, To exit press CTRL+C')
-channel.start_consuming()
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
